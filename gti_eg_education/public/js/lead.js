ce1633bcec4e050c65452883a244265f9948402c
Mainul Islam <mainulkhan94@gmail.com>
2019-09-30 12:20:22 +0600

    Export Fields

--- gti_eg_education/public/js/lead.js
+++ gti_eg_education/public/js/lead.js
@@ -1,1 +1,15 @@
 {% include "gti_eg_education/public/js/follow_up.js" %}
+
+frappe.ui.form.on("Lead", {
+	onload: frm => {
+		frm.questions = {}
+		frappe.call({
+
+		})
+	}
+})
+
+
+const set_question_answer = (frm, cdt, cdn) => {
+
+}
