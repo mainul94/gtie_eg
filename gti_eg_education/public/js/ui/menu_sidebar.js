8dddd7c35405bf093f63c761ca3ecd85eb01d8d8
Mainul Islam <mainulkhan94@gmail.com>
2019-10-03 02:33:32 +0600

    Make FIrst Face Menu

--- gti_eg_education/public/js/ui/menu_sidebar.js
+++ gti_eg_education/public/js/ui/menu_sidebar.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
 class MenuSidebar {
 	constructor () {
 		this.$wrapper = $(`<aside>`).prependTo('#body_div')
-		this.$wrapper.html("Hello")
 		this.render_root_menu()
 	}
 	render_root_menu () {
+		this.$root_menu_wrp = $(`<ul>`).addClass('left_menu list-group').appendTo(this.$wrapper)
 		if(!gti_eg['menu']) {
 			this.get_root_menu('', 1).then(results => {
 				gti_eg['menu'] = results
@@ -21,18 +21,78 @@ class MenuSidebar {
 		}
 	}
 	render_parent_menu () {
-		gti_eg.menu.forEach(menu=>{
-			this.$wrapper[menu.module_name] = $(`<li>`).html(menu.module_name).appendTo(this.$wrapper)
+		$.each(gti_eg['menu'], (key, rows)=>{
+			this.$wrapper[key] = $(`<li class="list-group-item disabled">`).html(key).appendTo(this.$root_menu_wrp)
+			rows.forEach(menu=>{
+				this.$wrapper[key][menu.module_name] = $(`<li class="list-group-item" data-module="${menu.module_name}" data-render-children=0>`).html(this.get_root_item_html(menu)).appendTo(this.$root_menu_wrp)
+				this.$wrapper[key][menu.module_name].hover(el=>{
+					let $el = $(el.target)
+					if (!$el.is('[data-render-children]'))
+						return;
+					if (parseInt($el.attr('data-render-children'))===0) {
+						$($el).attr('data-render-children', 1)
+						let $menu = $(`<ul class="menu loader list-group">`).appendTo($el)
+						this.get_root_menu($($el).attr('data-module')).then(results=>{
+							this.render_menu($menu, results.data)
+							$menu.removeClass('loader')
+						})
+					}
+				})
+			})
 		})
+	}
+	render_menu($parent, items) {
+		items.forEach(rows=>{
+			$parent.append(`<li class="list-group-item disabled">${rows.label}</li>`)
+			rows.items.forEach(item=>{
+				$parent.append(`<li class="list-group-item">${this.get_item_html(item)}</li>`)
+			})
+		})
+	}
+	get_item_html(module) {
+		let $html = `<a  title="${module.label}" href="${this.get_link(module)}"><span title="${module.label}">
+		${module.icon? '<i class="'+ module.icon +'"></i>': ""}
+		${module.label}</a></span>`
+		return $html
+	}
+	get_root_item_html(module) {
+		let $html = `<span title="${module.label}">
+		${module.icon? '<i class="'+ module.icon +'"></i>': ""}
+		${module.label}<a  title="${module.label}" href="${this.get_link(module)}">
+		<i class="fa fa-caret-right"></i>
+		</a></span>`
+		return $html
+	}
+	get_link(item) {
+		if (item.type==="module"){
+			return `#modules/${item.module_name}`
+		}
+		else if (item.type==="doctype") {
+			if(!item.settings){
+				return `#List/${item.name}`
+			}else {
+				return `#Form/${item.name}`
+			}
+		}
+		else if (item.type==="report") {
+			if (item.is_query_report) {
+				return `#query-report/${item.name}`
+			}else {
+				return `#List/${item.doctype}/Report/${item.name}`
+			}
+		}
 	}
 	get_root_menu (parent, is_root) {
 		return new Promise((resolve, reject)=>{
+			let args = {
+				parent: parent
+			}
+			if (is_root) {
+				args['is_root'] = true
+			}
 			frappe.call({
 				"method": "gti_eg_education.utils.menu.get_menu",
-				args: {
-					is_root: is_root,
-					parent: parent
-				},
+				args: args,
 				callback: r => {
 					if (r['message']) {
 						resolve(r.message)
--- gti_eg_education/utils/menu.py
+++ gti_eg_education/utils/menu.py
@@ -4,6 +4,6 @@ from frappe.desk.moduleview import get_desktop_settings, get
 @frappe.whitelist()
 def get_menu(parent=None, is_root=False):
 	if is_root:
-		return get_desktop_settings().get('Modules')
+		return get_desktop_settings()
 	elif parent:
 		return get(parent)
